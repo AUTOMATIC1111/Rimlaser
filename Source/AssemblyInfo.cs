d2058427a18a5369ba6786ec9eda551a6d4bca87
AUTOMATIC1111 <16777216c@gmail.com>
2018-08-07 08:16:24 +0300

    version change

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.86.1805")]
-[assembly: AssemblyFileVersion("1.0.86.1805")]
+[assembly: AssemblyVersion("1.0.91.0759")]
+[assembly: AssemblyFileVersion("1.0.91.0759")]
